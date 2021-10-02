b79a8f7200f898543d4ceb886696b82061172b6a
Adam Pratt <adam.pratt@zocdoc.com>
2021-10-02 14:41:06 -0400

    feat: fixed mobile for homepage

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Footer from '../components/Footer';
+import WorkSection from '../components/WorkSection'
 import { getImageByName } from '../utils/imgLoader'
 
 export default function Home() {
@@ -23,31 +24,7 @@ export default function Home() {
             and research to identify, prevent, and treat traumatic brain injury and concussion secondary to the experience of 
             domestic, intimate partner, and family violence.
         </p>
-        <div className="work-container">
-          <h1 className="work-title">Our Work</h1>
-          <div className="work-icon-container">
-            <img className="work-icon-left" width="120px" height="120px" src={getImageByName('icon1.png')}></img>
-            <img className="work-icon-middle" width="140px" height="120px" src={getImageByName('icon2.png')}></img>
-            <img className="work-icon-right" width="140px" height="120px" src={getImageByName('icon3.png')}></img>
-          </div>
-          <div className="work-subtitle-container">
-            <div className="work-subtitle">Resources</div>
-            <div className="work-subtitle">Community</div>
-            <div className="work-subtitle">Advocacy</div>
-          </div>
-          <div className="work-text-container">
-            <div className="work-text">Provide innovative concussion screening tools
-            for individuals and first responders to aid in the detection of concussion from
-            domestic, intimate partner, and family violence</div>
-            <div className="work-text">Uniting healthcare workers, first responders,
-            policy makers, the legal system, the public at large, and anyone who has
-            experienced violence where they live</div>
-            <div className="work-text">We are a source of education for leaders and
-            policymakers on identifying, preventing, and treating concussion
-            and traumatic brain injury as a result of domestic,
-            intimate partner, and family violence</div>
-          </div>
-        </div>
+        {WorkSection()}
         <div className='join-us-container'>
           <div className='join-us-words-container'>
             <h1 className='join-us-title'>Join Us</h1>
@@ -69,24 +46,6 @@ export default function Home() {
       </footer>
 
       <style jsx>{`
-        .work-icon-left {
-          margin: 0 10% 0 3%;
-          width: 120px;
-          height: 120px;
-        }
-        .work-icon-middle {
-          margin: 0 14% 0 14%;
-          width: 140px;
-          height: 120px;
-        }
-        .work-icon-right {
-          margin: 0 3% 0 10%;
-          width: 120px;
-          height: 120px;
-        }
-        .work-icon-container {
-          justify-content: space-between;
-        }
         .homepageImage {
           display: block;
           max-width:100%;
@@ -94,24 +53,7 @@ export default function Home() {
           height: auto;
         }
 
-        .work-text-container {
-          display: flex;
-          margin-bottom: 3%;
-          margin-top: 3%;
-        }
-        .work-subtitle {
-          font-size: 42px;
-          font-weight: 390;
-          color: white;
-          margin: 0 10% 0 10%;
-        }
-        .work-subtitle-container {
-          background-color: #370F7A;
-          display: flex;
-          flex-direction: row;
-          justify-content: center;
-        }
-
+       
         .bannerWrapper {
           width: 100%;
           padding: 10px 0 10px 0;
@@ -138,49 +80,13 @@ export default function Home() {
           font-size: 60px;
         }
 
-        .work-text {
-          flex-basis: 100%;
-          text-align: center;
-          font-weight: 390;
-          word-spacing: 2px;
-          color: black;
-          letter-spacing: 1px;
-          font-size: 28px;
-          margin: 0 1% 0 1%;
-        }
-        .work-header {
-          font-style: italic;
-          font-size: 50px;
-        }
-        .work-title {
-          margin: 0 0 24px 0;
-          font-size: 60px;
-        }
-        .work-box {
-          padding: 0 10px 0 10px;
-          width 33.33%;
-          height: 100%;
-        }
-        .work-box-container {
-          width 100%;
-          height: 400px;
-          display: flex;
-          align-items: center;
-        }
-        .work-container {
-          text-align: center;
-          color: white;
-          width: 100%;
-        }
+        
         .mission-title {
           color: #370F7A;
           font-size: 50px;
           margin-bottom: 0px;
         }
-        .work-title {
-          color: #370F7A;
-          font-size: 50px;
-        }
+     
         .mission-text {
           width: 80%;
           font-weight: 390;
