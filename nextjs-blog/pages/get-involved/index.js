618c853ca41aa1ddecb62207ef037c675173afae
Adam Pratt <adam.pratt@zocdoc.com>
2021-10-01 23:01:39 -0400

    feat: form stlying

--- nextjs-blog/pages/get-involved/index.js
+++ nextjs-blog/pages/get-involved/index.js
@@ -9,9 +9,31 @@ export default function GetInvolved() {
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
     const handleSubmit = (event) => {
-        console.log(name, email, message)
+
         event.preventDefault();
-        alert('The name you entered was: ${name}')
+        fetch('https://jbywqajk4h.execute-api.us-west-1.amazonaws.com/Prod/submitForm', {
+            method: 'POST',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                message: message,
+            })
+        }).then(response => {
+            const status = response.status
+            console.log(status)
+            const ele = document.getElementById('response');
+            if(status === 200) {
+                ele.innerHTML='Success, form submitted! You will hear back from us shortly'
+                setName("");
+                setEmail("");
+                setMessage("");
+            } else {
+                ele.innerHTML='Sorry, there was an error submitting your form, please email edie@safelivingspace.org directly'
+            }
+        })
+        
+        
     }
     return (
         <div className='get-involved-container'>
@@ -55,20 +77,20 @@ export default function GetInvolved() {
                 intimate partner, and family violence.
             </div>
             <form onSubmit={handleSubmit}>
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="name" value={name} class="form-control"  placeholder="Your name" onChange={(e)=> setName(e.target.value)}/>
+                <div className="form-group">
+                    <div className="input-name">Name</div>
+                    <input type="name" value={name} className="form-input-short"  placeholder="Your name" onChange={(e)=> setName(e.target.value)}/>
                 </div> 
-                <div class="form-group">
-                    <label for="email">Email address</label>
-                    <input type="email" class="form-control" placeholder="Enter email" onChange={(e)=> setEmail(e.target.value)}/>
+                <div className="form-group">
+                    <div className="input-name">Email address</div>
+                    <input type="email" className="form-input-med" placeholder="Enter email" onChange={(e)=> setEmail(e.target.value)}/>
                 </div>
-                <div class="form-group">
-                    <label for="name">Message</label>
-                    <input type="name" value={message} class="form-control"  placeholder="Tell us something" onChange={(e)=> setMessage(e.target.value)}/>
+                <div className="form-group">
+                    <div className="input-name">Message</div>
+                    <textarea resize="none" type="name" rows="4" value={message} className="form-input-long"  placeholder="Tell us something" onChange={(e)=> setMessage(e.target.value)}/>
                 </div>  
-                <h4 id="response"></h4>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <h4 id="response" className="response"></h4>
+                <button type="submit" className="submit-button">Submit</button>
             </form>
             <br></br>
             <br></br>
@@ -77,6 +99,53 @@ export default function GetInvolved() {
                 {Footer()}
             </footer>
             <style jsx> {`
+                .response {
+                    color: #370F7A;
+                }
+                .submit-button {
+                    width: 100%;
+                    background-color: #370F7A;
+                    color: white;
+                    padding: 14px 20px;
+                    margin: 8px 0;
+                    border: none;
+                    border-radius: 4px;
+                    cursor: pointer;
+                }
+                .submit-button:hover {
+                    opacity: 0.8;
+                }
+                .form-input-short {
+                    height: 40px;
+                    width: 200px;
+                }
+                .form-input-med {
+                    height: 40px;
+                    width: 400px;
+                }
+                .form-input-long {
+                    height: 80px;
+                    width: 600px;
+                    resize: none;
+                }
+                .form-group {
+                    display: flex;
+                    flex-direction: column;
+                    width: 600px;
+                    margin-top: 20px;
+                }
+
+                .input-name {
+                    margin-top: 15px;
+                    margin-bottom: 15px;
+                    width: 80%;
+                    font-weight: 390;
+                    word-spacing: 2px;
+                    color: #370F7A;
+                    letter-spacing: 1px;
+                    display: flex;
+                    font-size: 24px;
+                }
                 .venmo-header {
                     text-align: center;
                     padding-top: 20px;
@@ -173,6 +242,7 @@ export default function GetInvolved() {
                 }
 
                 .text {
+                    margin-bottom: 30px;
                     width: 80%;
                     font-weight: 390;
                     word-spacing: 2px;
