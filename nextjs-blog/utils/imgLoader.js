7999a53d4b3be60c855cc9288319bb57cc1a62e0
Adam Pratt <adam.pratt@zocdoc.com>
2022-04-02 11:09:42 -0400

    feat: skeleton of github actions and route file fetches through edge

--- nextjs-blog/utils/imgLoader.js
+++ nextjs-blog/utils/imgLoader.js
@@ -1,3 +1,3 @@
 export const getImageByName = (imgName) => {
-    return `http://safelivingspace.org.s3-website-us-west-1.amazonaws.com/images/${imgName}`
+    return `http://safelivingspace.org/images/${imgName}`
 }
